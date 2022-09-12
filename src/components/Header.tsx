7faebaf415c681e327167c1d29ef7b546676ba23
Wesley Alexandre <wesley.lopes14@hotmail.com>
2022-09-12 17:52:23 -0300

    Enviando os arquivos para o git

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    Image,
+    View
+} from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export function Header(){
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text>Olá</Text>
+                <Text>Rodrigo</Text>
+
+
+            </View>
+
+            <Image source={} />
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight()
+    }
+});
